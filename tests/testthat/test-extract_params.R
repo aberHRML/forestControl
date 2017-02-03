f925f4d78146d8bca20ba8c2f846fe05c02c10c7
wilsontom <wilsontom89@gmail.com>
2017-02-03 18:03:05 +0000

    update tests

--- tests/testthat/test-extract_params.R
+++ tests/testthat/test-extract_params.R
@@ -6,15 +6,15 @@ test_that("parameter-extraction",{
   rf_no_forest <- randomForest::randomForest(iris[,-5], factor(iris$Species), ntree = 100, keep.forest = FALSE)
   rf_w_forest <- randomForest::randomForest(iris[,-5], factor(iris$Species), ntree = 100, keep.forest = TRUE)
 
-  ranger_no_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = FALSE)
-  ranger_w_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = TRUE)
+  ranger_no_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = FALSE,num.trees = 100)
+  ranger_w_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = TRUE, num.trees = 100)
 
 
-  expect_true(is.list(extract_fpr_params(rf_w_forest)))
-  expect_true(is.list(extract_fpr_params(ranger_w_forest)))
+  expect_true(is.list(extract_params(rf_w_forest)))
+  expect_true(is.list(extract_params(ranger_w_forest)))
 
-  expect_error(extract_fpr_params(rf_no_forest))
-  expect_error(extract_fpr_params(ranger_no_forest))
+  expect_error(extract_params(rf_no_forest))
+  expect_error(extract_params(ranger_no_forest))
 
 
 })

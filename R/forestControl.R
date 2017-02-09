a84bbddc294b57c33b068a4fef189161ba995ee2
wilsontom <wilsontom89@gmail.com>
2017-02-09 16:59:47 +0000

    package doc

---
+++ R/forestControl.R
@@ -0,0 +1,18 @@
+#' False Positive Rate Control in Selection Frequency for Random Forest
+#'
+#' This package is an implementation of the methods described by Ender Konukoglu and Melanie Ganz in
+#' \emph{Konukoglu, E. and Ganz, M., 2014. Approximate false positive rate control in selection frequency
+#' for random forest. arXiv preprint arXiv:1410.2838} \url{https://arxiv.org/abs/1410.2838}.
+#'
+#' Selection Frequency Threshold
+#'
+#'
+#' Selection Frequency Threshold False Positive Rate (SF-FPR) feature selection
+#'
+#'
+#' SF-FPR feature elimination
+#'
+#' @docType package
+#' @name forestControl-package
+
+NULL

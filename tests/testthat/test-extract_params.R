42ab0030bc645baaeb554d5f8e0465e0a5d799fa
wilsontom <wilsontom89@gmail.com>
2017-02-09 16:57:23 +0000

    update tests

--- tests/testthat/test-extract_params.R
+++ tests/testthat/test-extract_params.R
@@ -4,9 +4,11 @@ test_that("parameter-extraction",{
 
 
   rf_no_forest <- randomForest::randomForest(iris[,-5], factor(iris$Species), ntree = 100, keep.forest = FALSE)
+  set.seed(1234)
   rf_w_forest <- randomForest::randomForest(iris[,-5], factor(iris$Species), ntree = 100, keep.forest = TRUE)
 
   ranger_no_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = FALSE,num.trees = 100)
+  set.seed(1234)
   ranger_w_forest <- ranger::ranger(factor(iris$Species) ~., iris[,-5], write.forest = TRUE, num.trees = 100)
 
 

4ae6a76c433485570f6412df6456a9f51b4d1051
wilsontom <wilsontom89@gmail.com>
2017-02-14 16:19:23 +0000

    selection frequecny extractor

---
+++ R/selecFreqs.R
@@ -0,0 +1,42 @@
+#' Variable Selection Frequencies
+#'
+#'
+#' @param x
+#' @return data.frame
+#' @export
+
+
+selecFreqs <- function(x){
+
+if(is.rf(x) == FALSE & is.ranger(x) == FALSE){
+    stop(deparse(substitute(x)), " is not a valid randomForest or ranger object", call. = FALSE)
+}
+
+if(is.null(x$forest)){
+  stop(deparse(substitute(x)), " has no forest", call. = FALSE)
+}
+
+
+if(class(x) == "randomForest"){
+  var <- numeric(length(x$forest$ncat))
+  var_freqs <- table(x$forest$bestvar[x$forest$bestvar > 0])
+  var[as.numeric(names(var_freqs))] <- var_freqs
+  names(var) <- names(x$forest$xlevels)
+  var_df <- data.frame(freq = var)
+}
+
+
+if(class(x) == "ranger"){
+  var <- numeric(length(x$forest$independent.variable.names))
+  var_freqs <- table(unlist(x$forest$split.varIDs)[unlist(x$forest$split.varIDs) > 0])
+  var[as.numeric(names(var_freqs))] <- var_freqs
+  names(var) <- x$forest$independent.variable.names
+  var_df <- data.frame(freq = var)
+}
+
+
+return(var_df)
+}
+
+
+

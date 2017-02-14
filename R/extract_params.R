b1b704459404644ddea282441b2919c82425f7ac
wilsontom <wilsontom89@gmail.com>
2017-02-14 16:20:22 +0000

    inheritance checking

--- R/extract_params.R
+++ R/extract_params.R
@@ -18,36 +18,28 @@
 extract_params <- function(x)
   {
 
-  if(inherits(x, "randomForest") | inherits(x, "ranger")){
-      type <- class(x)
-  }else{
-      stop("Input object must be either ranomForest or ranger")
+  if(is.rf(x) == FALSE & is.ranger(x) == FALSE){
+    stop(deparse(substitute(x)), " is not a valid randomForest or ranger object", call. = FALSE)
   }
 
-  if(type == "randomForest"){
-      if(!is.null(x$forest)){
+  if(is.null(x$forest)){
+    stop(deparse(substitute(x)), " has no forest", call. = FALSE)
+  }
+
+
+  if(class(x) == "randomForest"){
           Fn <- x$mtry
           Fe <- nrow(x$importance)
           K <- round(mean(apply(x$forest$nodestatus,2,function(x)(length(which(x == 1))))), digits = 0)
           Tr <- x$forest$ntree
-          }else{
-        stop("randomForest object must have forest data run model using;
 
-                  keep.forest = TRUE", call. = FALSE)
-      }
   }
 
-  if(type == "ranger"){
-      if(!is.null(x$forest)){
+  if(class(x) == "ranger"){
           Fn <- x$mtry
           Fe <- x$num.independent.variables
           K <- round(mean(sapply(x$forest$split.varIDs, function(x)(length(which(x != 0))))), digits = 0)
           Tr <- x$num.trees
-      }else{
-        stop("ranger object must have forest data run model using;
-
-                  write.forest = TRUE", call. = FALSE)
-      }
   }
 
   return(list(Fn = Fn,Ft = Fe, K = K, Tr = Tr))
